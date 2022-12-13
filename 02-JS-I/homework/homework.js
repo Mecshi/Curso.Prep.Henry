acfa75c5bc2325326313a701be7e55286349132f
Mecshi <maxi.correab@gmail.com>
2022-12-13 14:57:28 -0300

    Finish

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,7 +28,6 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-
   return str;
   
 }
@@ -37,15 +36,12 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-
   return (x + y);
-
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-
   return (x - y);
   
 }
@@ -53,17 +49,13 @@ function resta(x, y) {
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-
   return (x * y);
-
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-
   return (x / y);
-
 }
 
 function sonIguales(x, y) {
@@ -75,7 +67,6 @@ function sonIguales(x, y) {
     return true;
 
   } else {
-
       return false;
   }
   
@@ -87,13 +78,9 @@ function tienenMismaLongitud(str1, str2) {
   // Tu código:
 
   if (str1.length == str2.length){
-
     return true;
-
   } else {
-
     return false;
-
   }
   
 }
@@ -102,13 +89,10 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-
   if (num < 90){
 
     return true;
-
   } else {
-
     return false;
   }
 }
@@ -118,21 +102,15 @@ function mayorQueCincuenta(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (num > 50) {
-
     return true;
-
   } else {
-
     return false;
-
   }
-
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-
   return (x % y);
 
   
@@ -142,15 +120,10 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-
   if (num % 2 == 0){
-
     return true;
-
   } else {
-
     return false;
-
   }
   
 }
@@ -159,25 +132,17 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-
   if (num % 2 !== 0){
-
     return true;
-
   } else {
-
     return false;
-
   }
-
 }
-
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-
   return Math.pow(num, 2);
   
 }
@@ -185,15 +150,12 @@ function elevarAlCuadrado(num) {
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-
   return Math.pow(num, 3);
-
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-
   return Math.pow(num, exponent);
   
 }
@@ -201,14 +163,12 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-
   return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-
   return Math.ceil(num);
 
 }
@@ -216,7 +176,6 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-
   return Math.random();
 }
 
@@ -225,22 +184,14 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-
   if (numero < 0){
-
     return "Es negativo";
-
   } else if (numero > 0){
-
       return "Es positivo";
-
   } else {
-
       return false;
-
   }
 }
-
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
@@ -318,8 +269,8 @@ function esVocal(letra){
     } else {
       return "Dato incorrecto";
       
-      } 
-      
+    }
+  
   } else {
     return "Dato incorrecto";
   }
